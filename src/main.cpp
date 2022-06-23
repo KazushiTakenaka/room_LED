3e4aa6906d63323459fbc843db1d298387a02e7f
KazushiTakenaka <take.kazushi@gmail.com>
2022-06-23 21:40:01 +0900

    commit

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,18 @@
 #include <Arduino.h>
+#include <WiFi.h>
+#include <time.h>
+#include <Espalexa.h>
+Espalexa espalexa;
+
+// WiFi設定
+
+// 時刻設定
+const char *ntpServer = "pool.ntp.org";
+const long gmtOffset_sec = 32400;
+const int  daylightOffset_sec = 0;
 
 void setup() {
-  // put your setup code here, to run once:
+  
 }
 
 void loop() {
